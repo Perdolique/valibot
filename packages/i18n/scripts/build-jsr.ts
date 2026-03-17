e6b6f8b049a9aaf829d7553e9b2a199b4923d382
Fabian Hiller <hillerfabian11@gmail.com>
2026-03-17 14:03:53 -0700

    Finalize Azerbaijani language support PR

--- packages/i18n/scripts/build-jsr.ts
+++ packages/i18n/scripts/build-jsr.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import jsr from '../jsr.json';
 import package_ from '../package.json';
 import ar from '../src/ar';
-import az from '../src/az.ts';
+import az from '../src/az';
 import ca from '../src/ca';
 import cs from '../src/cs';
 import de from '../src/de';
@@ -45,9 +45,9 @@ const languages = [
   es,
   fa,
   fr,
+  hu,
   id,
   it,
-  hu,
   ja,
   ko,
   kr,
